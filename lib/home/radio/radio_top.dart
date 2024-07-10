46fbaf8d0ac409a061a6886304b824d0d971596d
nbow2 <nmawg22@gmail.com>
2024-07-11 02:43:03 +0300

    Finished Sebha screen. added radio screen ui.

--- lib/home/radio/radio_top.dart
+++ lib/home/radio/radio_top.dart
@@ -1,11 +1,67 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:islami_app/My_theme/app_colors.dart';
 
 class RadioTap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/radio_logo.png'),
+              // Update the path to your radio image
+              SizedBox(height: 20),
+              Text(
+                'إذاعة القرآن الكريم',
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.skip_previous,
+                      size: 60,
+                      color: AppColors.primaryLightColor,
+                    ),
+                    onPressed: () {
+                      // Add your previous button functionality here
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.play_arrow,
+                      size: 60,
+                      color: AppColors.primaryLightColor,
+                    ),
+                    onPressed: () {
+                      // Add your play button functionality here
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.skip_next,
+                      size: 60,
+                      color: AppColors.primaryLightColor,
+                    ),
+                    onPressed: () {
+                      // Add your next button functionality here
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/home/sebha/sebha_tap.dart
+++ lib/home/sebha/sebha_tap.dart
@@ -1,11 +1,90 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:islami_app/My_theme/app_colors.dart';
 
-class SebhaTap extends StatelessWidget {
+class SebhaTap extends StatefulWidget {
+  @override
+  _SebhaTapState createState() => _SebhaTapState();
+}
+
+class _SebhaTapState extends State<SebhaTap> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.only(top: 40.0, bottom: 0.0),
+            child: Column(
+              children: [
+                Image.asset('assets/images/sebha_logo.png'),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                padding: EdgeInsets.all(30.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'عدد التسبيحات',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 23.0, horizontal: 20.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xffd4af80),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Text(
+                        '$_counter',
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _incrementCounter,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 7.0, vertical: 12.0),
+                        child: Text(
+                          'سبحان الله',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.primaryLightColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
